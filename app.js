ce3a45428f3690edc14a8f209e3243d1224e7554
Steve Tuan <vothuong197@gmail.com>
2017-12-03 08:18:00 +0800

    Add Procfile

--- app.js
+++ app.js
@@ -70,4 +70,8 @@ app.get('/object/', (req, res) => {
 	});
 })
 
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
